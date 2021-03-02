bfc5a1566ba38442cf5b48b0429397935dc5fcbf
Alexandr Nevyantsev <anevyantsev@tradingview.com>
2021-03-02 20:52:05 +0300

    Remove custom decoding from SeriesType & remove case none in SeriesType

--- LightweightCharts/Classes/LightweightChartsModels/SeriesType.swift
+++ LightweightCharts/Classes/LightweightChartsModels/SeriesType.swift
@@ -1,18 +1,10 @@
 import Foundation
 
 
-public enum SeriesType: String {
+public enum SeriesType: String, Decodable {
     case line = "Line"
     case area = "Area"
     case candlestick = "Candlestick"
     case bar = "Bar"
     case histogram = "Histogram"
-    case none
-}
-
-extension SeriesType: Decodable {
-    public init(from decoder: Decoder) throws {
-        let type = try decoder.singleValueContainer().decode(String.self)
-        self = SeriesType(rawValue: type) ?? .none
-    }
 }
